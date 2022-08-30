4b3dea64989bc254d749eb0abb7c8a6903c09085
xanwil <97523333+xanwil@users.noreply.github.com>
2022-08-30 16:18:31 +0100

    bootstrap installed and header added

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+        <h1>Reddit Brighton & Hove</h1>
+        <input type="text" placeholder="Search.."></input>
+    </div>
+  )
+}
