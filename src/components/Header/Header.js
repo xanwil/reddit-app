505963fb45fe5772dc772a01230fbf8c923c2c1b
xanwil <97523333+xanwil@users.noreply.github.com>
2022-09-12 15:04:12 +0100

    styles started

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,9 @@
-import React from 'react'
-
+import React from 'react';
+import "./Header.css";
 
 export default function Header() {
   return (
-    <div>
+    <div className='header'>
         <h1>Reddit B&H</h1>
         <input type="text" placeholder="Search.."></input>
     </div>
