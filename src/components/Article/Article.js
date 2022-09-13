47d2de76b873e8cc5955fdcc9a63b07af2bf7f26
xanwil <97523333+xanwil@users.noreply.github.com>
2022-09-13 18:21:53 +0100

    styling started

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,20 +1,33 @@
-import React from 'react'
+import React from "react";
+import "./Article.css";
 
 export default function Article(props) {
-    const { title, score, image, video, author, created, num_comments, secure_media } = props;
-
-
+  const {
+    title,
+    score,
+    image,
+    video,
+    author,
+    created,
+    num_comments,
+    secure_media,
+  } = props;
 
   return (
     <div>
-      <h3>{ title } </h3>
-      <h3>&uarr; { score } &darr;</h3>
-      <img src={ image } alt=""/>
-      {/* <video src={ video }></video> */}
-      <h5>Posted by { author }</h5>
-      <h5>{ created }</h5>
-      <h5> { num_comments }</h5> 
+      <div className="topRow">
+        <h3>&uarr; {score} &darr;</h3>
+        <h3>{title} </h3>
+      </div>
+      <div className="middleRow">
+        <img src={image} alt="" />
+        {/* <video src={ video }></video> */}
+      </div>
+      <div className="bottomRow">
+        <h5>Posted by {author}</h5>
+        <h5>{created}</h5>
+        <h5> {num_comments}</h5>
+      </div>
     </div>
-  )
+  );
 }
-
