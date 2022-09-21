e6281210d28b2bb65ed66116d2182c03ecf42fe0
xanwil <97523333+xanwil@users.noreply.github.com>
2022-09-21 10:39:08 +0100

    commit before change to mixins

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,21 +1,28 @@
-import React from 'react'
+import React from "react";
+import "./Comment.css";
 
 export default function Comment(props) {
-const { comment,
-        author,
-        created 
-      } = props;
-
+  const { comment, author, created } = props;
 
   return (
-    <div>
-      <div className='topRow'>
-        <h3 className='author'>{author}</h3>
-        <h3 className='created'>{created}</h3>
+    <div className="commentContainer">
+      <div className="topRow">
+        <div className="commentIndent">
+          
+        </div>
+        <div className="commentDetails">
+          <h3 className="author">{author}</h3>
+          <h3 className="created">{created}</h3>
+        </div>
       </div>
-      <div className='bottomRow'>
-        <h3 className='comment'>{comment}</h3>
+      <div className="bottomRow">
+        <div className="commentIndent">
+          
+        </div>
+        <div className="commentDetails">
+          <h3 className="comment">{comment}</h3>
+        </div>
       </div>
     </div>
-  )
+  );
 }
