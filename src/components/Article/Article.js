60757d6e55890408f58d8bbccb2af599b07c24cc
xanwil <97523333+xanwil@users.noreply.github.com>
2022-09-28 12:56:45 +0100

    burger menu started

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import "./Article.css";
 import Comments from "../Comments/Comments";
 
-
 export default function Article(props) {
   const {
     title,
@@ -13,54 +12,58 @@ export default function Article(props) {
     created,
     num_comments,
     secure_media,
-    permalink
+    permalink,
   } = props;
 
   const [showComments, setShowComments] = useState(false);
 
   // const getRealComments = (num) => num > 0 ? num : 0;
 
-
   return (
     <div>
-    <div className="article">
-      <div className="topRow">
-        <div className="score">
-          <h5>&uarr; </h5>
-          <h5>{score} </h5>
-          <h5>&darr;</h5>
+      <div className="article">
+        <div className="topRow">
+          <div className="score">
+            <h5>&uarr; </h5>
+            <h5>{score} </h5>
+            <h5>&darr;</h5>
+          </div>
+          <p className="title">{title}</p>
         </div>
-        <p className="title">{title}</p>
-      </div>
-      <div className="middleRow">
-        <div className="indent"></div>
-        <div className="image">
-        {/* <div className="indent"></div> */}
-        <img src={image} alt="" />
-        {/* <video src={ video }></video> */}
+        <div className="middleRow">
+          <div className="indent"></div>
+          <div className="image">
+            {/* <div className="indent"></div> */}
+            <img src={image} alt="" />
+            {/* <video src={ video }></video> */}
+          </div>
+        </div>
+        <div className="bottomRow">
+          <div className="indent"></div>
+          <div className="details">
+            <h5 className="detailsItem1">Posted by {author}</h5>
+            <div className="detailsRight">
+              <h5 className="detailsItem2">{created}</h5>
+              <h5 className="detailsItem3">
+                <span
+                  onClick={() => {
+                    setShowComments(!showComments);
+                  }}
+                  className="commentsLink"
+                >
+                  <i className="fas fa-comment-alt"></i>
+                  <span className="num_comments"> {num_comments}</span>
+                </span>
+              </h5>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="bottomRow">
-        <div className="indent"></div>
-        <div className="details">
-          <h5 className="detailsItem1">Posted by {author}</h5>
-          <h5 className="detailsItem2">{created}</h5>
-          <h5 className="detailsItem3">
-            <span
-              onClick={() => {
-                setShowComments(!showComments);
-              }}            
-              className="commentsLink"
-            >
-              <i className="fas fa-comment-alt"></i><span className="num_comments"> {num_comments}</span>
-            </span>
-          </h5>
-        </div>
-      </div>
-      </div>
-      {showComments ? <Comments permalink={permalink} num_comments={num_comments}/> : null}
+      {showComments ? (
+        <Comments permalink={permalink} num_comments={num_comments} />
+      ) : null}
     </div>
   );
 }
 
-// && num_comments > 0 
+// && num_comments > 0
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,11 +4,23 @@ import "./Header.css";
 export default function Header() {
   return (
     <div className="header">
-      <h1><span className="pink"><i className="fa fa-reddit"></i> Reddit</span><span className="black">Baking</span></h1>
+      <h1>
+        <span className="pink">
+          <i className="fa fa-reddit"></i> Reddit
+        </span>
+        <span className="black">Baking</span>
+      </h1>
       <form className="inputContainer">
         <input type="text" placeholder="Search.." name="search"></input>
-        <button type="submit"><i className="fa fa-search"></i></button>
+        <button type="submit">
+          <i className="fa fa-search"></i>
+        </button>
       </form>
+      <div className="hamburger">
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
     </div>
   );
 }
