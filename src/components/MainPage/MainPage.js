fb4a0d4ead96a63a7fec2722f06393bf1a3dc22a
xanwil <97523333+xanwil@users.noreply.github.com>
2022-10-03 18:17:49 +0100

    main page refactored

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,22 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import Articles from "../Articles/Articles";
 import Suggestions from "../Suggestions/Suggestions";
 // import ProgressBar from "../ProgressBar/ProgressBar";
-import { ThingsProvider } from "../thingsContext";
-
-
 import "./MainPage.css";
 
 export default function MainPage() {
-  // const [showComments, setShowComments] = useState(false);
 
   return (
     <div className="mainPage">
-    {/* <ThingsProvider value={{setShowComments, showComments}}> */}
-
       <Articles />
       <Suggestions />
-      {/* </ThingsProvider> */}
     </div>
   );
 }
